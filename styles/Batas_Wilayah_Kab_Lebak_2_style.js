1f32a24baa63e2392c61e93c19082ae0e4730594
yayan-19 <yayanr585@gmail.com>
2026-09-16 15:24:27 +0700

    Add files via upload

---
+++ styles/Batas_Wilayah_Kab_Lebak_2_style.js
@@ -0,0 +1,33 @@
+var size = 0;
+var placement = 'point';
+
+var style_Batas_Wilayah_Kab_Lebak_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(221,210,18,1.0)', lineDash: [5.699999999999999,1.14,2.28,1.14,2.28,1.14], lineCap: 'butt', lineJoin: 'miter', width: 1.14}),fill: new ol.style.Fill({color: 'rgba(206,200,11,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
